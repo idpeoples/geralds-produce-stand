1c770a9b04eaa58b46cee786bbd75deaa26da6d3
nucleareaglefox <nucleareaglefox@gmail.com>
2021-05-30 22:53:55 -0400

    add explanatory comments

--- install.py
+++ install.py
@@ -8,6 +8,7 @@ import random
 
 CONFIGURATION_FILE = "./configuration.json"
 
+# get_configuration: Extracts data from the json file, returning the main dictionary object
 def get_configuration():
 
     with open(CONFIGURATION_FILE) as configuration_file:
@@ -16,17 +17,22 @@ def get_configuration():
 
 def main():
 
+    # Get the configuration data and separate into two objects for readability
     configuration = get_configuration()
     credentials = configuration['database']['credentials']
     schema = configuration['database']['schema']
 
     with mariadb.connect(user = credentials["username"], password = credentials["password"], database = credentials["name"], host = 'localhost') as connection:
 
+        # MariaDB documentation says autocommit default is True, testing says otherwise; set True here to avoid confusion and missed insert
         connection.autocommit = True
         cursor = connection.cursor()
+
         create_table_query = "create table {:s} ({:s} int unsigned not null primary key auto_increment, {:s} text not null, {:s} int unsigned not null)"
         create_table_query = create_table_query.format(schema['table name'], schema['id'], schema['name'], schema['count'])
         cursor.execute(create_table_query)
+
+        # Insert a random value into the table to ensure we have something to work with
         insert_default_data_query = "insert into {:s} values (NULL, \"banana\", {:d})".format(schema['table name'], random.randint(1, 1000))
         cursor.execute(insert_default_data_query)
 
