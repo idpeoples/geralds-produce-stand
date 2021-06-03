db007bf9b460d6bd58bdb59249c46dd5f84c1230
nucleareaglefox <nucleareaglefox@gmail.com>
2021-06-02 22:00:37 -0400

    add basic homepage structure

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+
+    <head>
+
+        <link rel="stylesheet" href="index.css">
+        <title>Gerald's Produce Stand</title>
+
+    </head>
+
+    <body>
+
+        <div id="page-title" class="content">
+
+            <p>Gerald's Produce Stand</p>
+
+        </div>
+
+        <div id="navbar" class="content">
+
+            Home, Stock, Updates
+
+        </div>
+
+        <div id="carousel" class="content">
+
+        </div>
+
+    </body>
+
+</html>
